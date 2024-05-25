fa92f81dc10267e9ae73ce6122854e9fd4caced9
eduard-biris <eduardandreibiris@yahoo.com>
2024-05-25 12:52:46 +0300

    Modified routes - now there is only a generic endpoint for creating visualizations

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { storeVisualisation } = require('../services/visualisationService');
+const { storeVisualisation, readVisualisations } = require('../services/visualisationService');
 
-router.post('/timeline', async (req, res) => {
-    const util = require('util');
-    console.log('Got data: ', util.inspect(req.body, false, 30, true));
+router.get('/visualisations', async (req, res) => {
+    const filter = req.body?.filter ?? {};
 
-    const result = await storeVisualisation('timeline', req.body);
+    const result = await readVisualisations(filter);
 
-    res.send({
-        success: true,
-        dbResponse: result,
-    });
+   res.send({
+    visualisations: result
+   });
+});
+
+router.post('/visualization', async (req, res) => {
+    const result = await storeVisualisation(req.body);
+    res.send(result);
 });
 
 module.exports = router;
--- services/visualisationService.js
+++ services/visualisationService.js
@@ -1,14 +1,19 @@
 const database = require('../persistence').getDatabase();
 
-const storeVisualisation = async (type, visualisationData) => {
-    const result = await database.collection('test_collection').insertOne({
-        type,
-        ...visualisationData,
-    });
+const storeVisualisation = async (visualisation) => {
+    const result = await database.collection('test_collection').insertOne(visualisation);
+
+    return result;
+};
+
+const readVisualisations = async (filter = {}) => {
+    const resultCursor = await database.collection('test_collection').find(filter);
+    const result = await resultCursor.toArray();
 
     return result;
 };
 
 module.exports = {
     storeVisualisation,
+    readVisualisations
 };
